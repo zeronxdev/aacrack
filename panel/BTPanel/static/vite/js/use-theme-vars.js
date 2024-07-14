02617de133c53bbdf0c2b13271dec4f113645cad
ZeronX <160329349+zeronxdev@users.noreply.github.com>
2024-07-14 10:55:54 +0000

    update

---
+++ panel/BTPanel/static/vite/js/use-theme-vars.js
@@ -0,0 +1,1 @@
+import{cg as e,aD as n}from"./index.js?v=1720690738171";import{a as o,k as r}from"./vue.js?v=1720690738171";function m(){const m=o(e,null);return r((()=>{if(null===m)return n;const{mergedThemeRef:{value:e},mergedThemeOverridesRef:{value:o}}=m,r=(null==e?void 0:e.common)||n;return(null==o?void 0:o.common)?Object.assign({},r,o.common):r}))}export{m as u};
